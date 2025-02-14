de5acfb205f7bda4c7d291d7affdce21456e9c2f
Simon turan <simon.turan@telekom.de>
2025-02-14 13:53:19 +0100

    domain classes + Controller

---
+++ src/main/java/de/tomsandt/salesync/domain/Customer.java
@@ -0,0 +1,79 @@
+package de.tomsandt.salesync.domain;
+
+public class Customer {
+
+    private long id;
+    private String lastName;
+    private String firstName;
+    private String street;
+    private String zipCode;
+    private String city;
+    private String eMail;
+    private String phone;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEMail() {
+        return eMail;
+    }
+
+    public void setEMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
+
+
